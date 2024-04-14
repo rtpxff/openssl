50f4494bac0b59f5225df8a4439c60c2b4c25634
palka.projekt <palka.projekt@gmail.com>
2024-04-14 23:08:12 +0200

    invisible cmds

--- vcxp.bat
+++ vcxp.bat
@@ -1,12 +1,12 @@
-del vc
-del vc.in
+@del vc
+@del vc.in
 
-del *.vcxproj
-del test\*.vcxproj
-del fuzz\*.vcxproj
-del apps\*.vcxproj
-del providers\*.vcxproj
-del util\*.vcxproj
+@del *.vcxproj
+@del test\*.vcxproj
+@del fuzz\*.vcxproj
+@del apps\*.vcxproj
+@del providers\*.vcxproj
+@del util\*.vcxproj
 call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
 perl Configure VC-WIN64B
 
