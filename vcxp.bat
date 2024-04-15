2f5851e66e1ecd761da1e242a55584dc1e5dfad0
palka.projekt <palka.projekt@gmail.com>
2024-04-15 23:23:36 +0200

    info

--- vcxp.bat
+++ vcxp.bat
@@ -14,16 +14,4 @@
 @copy vc.in makefile.in
 @nmake
 
-for %a in      (test\*.vcxproj) do ren "%~a"      "test_%~na%~xa"
-for %a in      (fuzz\*.vcxproj) do ren "%~a"      "fuzz_%~na%~xa"
-for %a in      (apps\*.vcxproj) do ren "%~a"      "apps_%~na%~xa"
-for %a in (providers\*.vcxproj) do ren "%~a" "providers_%~na%~xa"
-for %a in      (util\*.vcxproj) do ren "%~a"      "util_%~na%~xa"
-
-@move      test\*.vcxproj .
-@move      fuzz\*.vcxproj .
-@move      apps\*.vcxproj .
-@move providers\*.vcxproj .
-@move      util\*.vcxproj .
-
 pause
